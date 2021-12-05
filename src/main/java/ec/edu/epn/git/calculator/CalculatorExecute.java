635a27b254c3f8548851a8d434af86c35242c2ce
tapiaalexv <alexis.tapia@epn.edu.ec>
2021-12-05 18:51:53 -0500

    Adding jar

---
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -0,0 +1,15 @@
+package ec.edu.epn.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        System.out.println("== Calculator Execute ==");
+
+        Calculator c = new Calculator();
+
+        int addition = c.addition(4,7);
+        System.out.println("c.addition(4,7) -> "+addition);
+
+        int substraction = c.subtraction(7,1);
+        System.out.println("c.substraction(7,1) -> "+substraction);
+    }
+}
